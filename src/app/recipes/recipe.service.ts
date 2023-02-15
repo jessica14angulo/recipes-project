819055a624efdcbc9eb85117b640cbd3e9cc8e38
jessica14angulo <ang21016@byui.edu>
2023-02-15 12:26:36 -0400

    Observables Completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 import { Injectable } from "@angular/core";
@@ -7,7 +7,6 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe(
             'Tteokbokki', 
@@ -23,12 +22,12 @@ export class RecipeService {
             'Jjajangmyeon', 
             'Taste it once and you will never stop', 
             'https://i.ytimg.com/vi/F4Cm75Qvk4A/maxresdefault.jpg', 
-        [
-            new Ingredient('cup of Korean radish (or daikon)',1),
-            new Ingredient('cup of zucchini, cut into ½ inch cubes',1),
-            new Ingredient('cup of potato, peeled and cut into ½ inch cubes',1),
-            new Ingredient('cup of onion chunks',1)
-        ])
+            [
+                new Ingredient('cup of Korean radish (or daikon)',1),
+                new Ingredient('cup of zucchini, cut into ½ inch cubes',1),
+                new Ingredient('cup of potato, peeled and cut into ½ inch cubes',1),
+                new Ingredient('cup of onion chunks',1)
+            ])
       ]; 
 
     constructor(private slService: ShoppingListService) {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Teokk', 10),
         new Ingredient('Gochujang', 1),
@@ -15,11 +16,11 @@ export class ShoppingListService {
 
     addingredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
     addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
